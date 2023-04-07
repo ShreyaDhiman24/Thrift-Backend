4222b8768d3f40660eedcee42ecead62e10ba8bc
Shreya Dhiman <shreyanksha2004@gmail.com>
2023-04-08 02:06:01 +0530

    public modified

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate, useNavigation } from "react-router-dom";
-import Button from 'react-bootstrap/Button';
+import { useNavigate } from "react-router-dom";
 import Card from 'react-bootstrap/Card';
-import { ChakraProvider, Skeleton, SkeletonText } from "@chakra-ui/react";
+import { ChakraProvider} from "@chakra-ui/react";
 import '../css/Card.css';
 
 
